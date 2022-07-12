4b7bde909fa2b2b52687431acb265f3152449c4d
Michelle Fox-Romero <michelle.fox@michell-ltmdryt.internal.salesforce.com>
2022-07-12 16:36:19 -0700

    implemented exercise page, workouts not logging

--- ui/src/components/ExercisePage/ExercisePage.jsx
+++ ui/src/components/ExercisePage/ExercisePage.jsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import "./ExercisePage.css";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import ExerciseOverview from "../ExerciseOverview/ExerciseOverview";
+import ExerciseNew from "../ExerciseNew/ExerciseNew";
+import ExerciseDetail from "../ExerciseDetail/ExerciseDetail";
 import LoginForm from "../LoginForm/LoginForm";
 
 export default function ExercisePage(props) {
@@ -11,6 +15,29 @@ export default function ExercisePage(props) {
           <div className="banner">
             <h1>Exercise</h1>
           </div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ExerciseOverview
+                  exercise={props.exercise}
+                  user={props.user}
+                ></ExerciseOverview>
+              }
+            ></Route>
+            <Route
+              path="/create"
+              element={
+                <ExerciseNew addExercise={props.addExercise}></ExerciseNew>
+              }
+            ></Route>
+            <Route
+              path="/id/:exerciseId"
+              element={
+                <ExerciseDetail exercise={props.exercise}></ExerciseDetail>
+              }
+            ></Route>
+          </Routes>
         </main>
       ) : (
         <LoginForm user={props.user} setUser={props.setUser}></LoginForm>
