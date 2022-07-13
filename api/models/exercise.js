0f48cfeda504f0398397ccf60b91b4e94b91bfba
Michelle Fox-Romero <michelle.fox@michell-ltmdryt.internal.salesforce.com>
2022-07-13 16:24:47 -0700

    Deploying express api

---
+++ api/models/exercise.js
@@ -0,0 +1,78 @@
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../utils/errors");
+
+class Exercise {
+  static async createExercise({ exercise, user }) {
+    const requiredFields = ["name", "category", "duration", "intensity"];
+    requiredFields.forEach((field) => {
+      if (!exercise.hasOwnProperty(field)) {
+        throw new BadRequestError(
+          `Required field - ${field} - missing from request body`
+        );
+      }
+    });
+    const results = await db.query(
+      `
+            INSERT INTO exercise (name, category, duration, intensity, user_id)
+            VALUES ($1, $2, $3, $4, (SELECT id FROM users WHERE email = $5))
+            RETURNING id, name, category, duration, intensity, user_id, created_at
+            `,
+      [
+        exercise.name,
+        exercise.category,
+        exercise.duration,
+        exercise.intensity,
+        user.email,
+      ]
+    );
+    return results.rows[0];
+  }
+
+  static async fetchExerciseById(exerciseId) {
+    const results = await db.query(
+      `
+            SELECT  e.id,
+                    e.name,
+                    e.category,
+                    e.duration,
+                    e.intensity,
+                    u.email AS "user_email",
+                    e.user_id AS "user_id",
+                    e.created_at AS "created_at"
+            FROM exercise AS e
+                JOIN users AS u ON u.id = e.user_id
+            WHERE e.id = $1
+            `,
+      [exerciseId]
+    );
+    const exercise = results.rows[0];
+    if (!exercise) {
+      throw new NotFoundError();
+    }
+    return exercise;
+  }
+
+  static async listExerciseForUser({ user_id }) {
+    const results = await db.query(
+      `
+            SELECT  e.id,
+                    e.name,
+                    e.category,
+                    e.duration,
+                    e.intensity,
+                    u.email AS "user_email",
+                    e.user_id AS "user_id",
+                    e.created_at AS "created_at"
+            FROM exercise AS e
+                JOIN users AS u ON u.id = e.user_id
+            WHERE e.user_id = $1
+            ORDER BY e.created_at DESC
+            `,
+      [user_id]
+    );
+    console.log(results.rows);
+    return results.rows;
+  }
+}
+
+module.exports = Exercise;
