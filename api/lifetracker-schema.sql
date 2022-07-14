cdb6feacb208fe1b49890662f3d737113e0e0741
Michelle Fox-Romero <michelle.fox@michell-ltmdryt.internal.salesforce.com>
2022-07-14 14:00:26 -0700

    final commit

--- api/lifetracker-schema.sql
+++ api/lifetracker-schema.sql
@@ -9,6 +9,17 @@ CREATE TABLE users (
     updated_at  TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE exercise (
+    id          SERIAL PRIMARY KEY,
+    name        TEXT NOT NULL,
+    category    TEXT NOT NULL,
+    duration    INT NOT NULL CHECK (duration > 0),
+    intensity   INT NOT NULL CHECK (intensity > 0 AND intensity <= 10),
+    user_id     INT NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 CREATE TABLE nutrition (
     id          SERIAL PRIMARY KEY,
     name        TEXT NOT NULL,
@@ -16,6 +27,15 @@ CREATE TABLE nutrition (
     quantity    INT NOT NULL DEFAULT 1,
     calories    INT NOT NULL,
     image_url   TEXT NOT NULL,
+    user_id     INT NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE sleep (
+    id          SERIAL PRIMARY KEY,
+    start_time  TIMESTAMP NOT NULL DEFAULT NOW(),
+    end_time    TIMESTAMP NOT NULL DEFAULT NOW(),
     user_id     INT NOT NULL,
     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
--- api/server.js
+++ api/server.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { NotFoundError } = require("./utils/errors");
+const { PORT } = require("./config");
+const security = require("./middleware/security");
 const authRoutes = require("./routes/auth");
+const nutritionRoutes = require("./routes/nutrition");
+const exerciseRoutes = require("./routes/exercise");
+const sleepRoutes = require("./routes/sleep");
+const activityRoutes = require("./routes/activity");
 
 const app = express();
-const PORT = 3001;
 
 app.use(cors());
-app.use(morgan("tiny"));
+
 app.use(express.json());
+
+app.use(morgan("tiny"));
+
+app.use(security.extractUserFromJwt);
+
 app.use("/auth", authRoutes);
+app.use("/nutrition", nutritionRoutes);
+app.use("/exercise", exerciseRoutes);
+app.use("/sleep", sleepRoutes);
+app.use("/activity", activityRoutes);
 
 app.use((req, res, next) => {
   return next(new NotFoundError());
